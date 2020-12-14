85fefdcc15941b9c986f499c8804a00a455237e7
unknown <dragonsuperf@gmail.com>
2020-12-14 11:30:12 +0900

    add travis.yml

---
+++ src/components/SearchForm.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import { Carousel } from 'react-bootstrap';
+import { Form, Col, Row, Container } from 'react-bootstrap';
+import styled from 'styled-components';
+
+class SearchForm extends Component {
+  render() {
+    return (
+      <Carousel>
+      </Carousel>
+    );
+  }
+}
